354cf31cae32c64d6709f76300c495d10621b969
Rui <gintatsu3@gmail.com>
2026-02-28 13:45:36 +0700

    Update quickshell-arch.sh

--- quickshell-arch.sh
+++ quickshell-arch.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
 
-# This script outputs the current date and time in UTC format
-# and the current user's login name.
-
-# Get the current date and time in UTC format
-current_date_time=$(date -u '+%Y-%m-%d %H:%M:%S')
-
-# Get the current user's login name
-current_user=$(whoami)
-
-# Output the results
-echo "Current Date and Time (UTC - YYYY-MM-DD HH:MM:SS formatted): $current_date_time"
-echo "Current User's Login: $current_user"
